9cb04d02fb1a8147b77ed9390b286478cfeeead8
Eric Ofman <eric.ofman@hotmail.com>
2016-02-07 22:32:29 -0500

    Parses and decodes video data

--- videodata.py
+++ videodata.py
@@ -9,6 +9,7 @@ class VideoData:
         
         url_base = 'https://www.youtube.com'
         request_url = url_base + '/get_video_info?video_id'
+        vevo_post_fix = '&el=vevo&el=embedded'
         watch_url =  url_base + '/watch?v'
         
         if('https://' not in self._video_url):
@@ -24,6 +25,32 @@ class VideoData:
             sys.exit("Invalid or unsupported YouTube URL: %s" % self._video_url)
         
         request = urlopen(request_url) 
-        self._video_info = request.read()
+        self._video_info = parse_qs(request.read())
         
-vinfo = VideoData('https://www.youtube.com/get_video_info?video_id=oThh3_Srxtc')
+        if(self.is_vevo()):
+            request_url += vevo_post_fix
+            request = urlopen(request_url) 
+            self._video_info = parse_qs(request.read())            
+            
+    def is_vevo(self):
+        reason = self._video_info[b'reason'][0].decode('utf-8')
+        if('VEVO' in reason):
+            return True
+        else:
+            return False
+    
+    def get_title(self): 
+        return self._video_info[b'title'][0].decode('utf-8')
+    
+    def get_thumbnail_url(self, hq):
+        thumbnail = self._video_info[b'thumbnail_url'][0].decode('utf-8') \
+            .split('/')
+        if(hq):
+            thumbnail[-1] = 'hq' + thumbnail[-1]
+            thumbnail = '/'.join(thumbnail)
+        return thumbnail
+
+if __name__ == '__main__':
+    vinfo = VideoData('https://www.youtube.com/watch?v=EgqUJOudrcM')
+    print(vinfo.get_title())
+    print(vinfo.get_thumbnail_url(True))
