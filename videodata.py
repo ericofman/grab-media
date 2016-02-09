8f60dfd8fb71b3570a45ed691e089c39161a8e46
Eric Ofman <eric.ofman@hotmail.com>
2016-02-09 01:43:49 -0500

    lists all the available streams

--- videodata.py
+++ videodata.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlparse 
 from urllib.parse import parse_qs 
+from urllib.parse import urlencode 
 from urllib.request import urlopen 
 import sys
 
@@ -9,8 +10,8 @@ class VideoData:
         
         url_base = 'https://www.youtube.com'
         request_url = url_base + '/get_video_info?video_id'
-        vevo_post_fix = '&el=vevo&el=embedded'
         watch_url =  url_base + '/watch?v'
+        vevo_postfix = '&el=vevo&el=embedded'
         
         if('https://' not in self._video_url):
             self._video_url = 'https://' + self._video_url
@@ -20,37 +21,45 @@ class VideoData:
         if(request_url in parse.keys()):
             request_url = request_url + '=' + parse[request_url][0]
         elif(watch_url in parse.keys()):
-            request_url = request_url + '=' +parse[watch_url][0]
+            request_url = request_url + '=' + parse[watch_url][0]
         else:
             sys.exit("Invalid or unsupported YouTube URL: %s" % self._video_url)
         
         request = urlopen(request_url) 
         self._video_info = parse_qs(request.read())
         
-        if(self.is_vevo()):
-            request_url += vevo_post_fix
-            request = urlopen(request_url) 
-            self._video_info = parse_qs(request.read())            
-            
-    def is_vevo(self):
-        reason = self._video_info[b'reason'][0].decode('utf-8')
-        if('VEVO' in reason):
-            return True
-        else:
-            return False
+        for key in self._video_info.keys():
+            self._video_info[key.decode('utf-8')] = self._video_info.pop(key) 
     
     def get_title(self): 
-        return self._video_info[b'title'][0].decode('utf-8')
+        return self._video_info['title'][0].decode('utf-8')
     
-    def get_thumbnail_url(self, hq):
-        thumbnail = self._video_info[b'thumbnail_url'][0].decode('utf-8') \
+    def get_thumbnail_url(self, quality):
+        ''' 
+        Thumbnail quality: default, hq, mq, sd, max
+        '''
+        thumbnail = self._video_info['thumbnail_url'][0].decode('utf-8') \
             .split('/')
-        if(hq):
-            thumbnail[-1] = 'hq' + thumbnail[-1]
-            thumbnail = '/'.join(thumbnail)
+        if(quality == "default"):
+            thumbnail[-1] = thumbnail[-1]
+        elif(quality in ['hq', 'mq', 'sd', 'max']):
+            thumbnail[-1] = quality + thumbnail[-1]
+        else:
+            sys.exit("Invalid thumbnail quality prefix: %s" % quality)
+        thumbnail = '/'.join(thumbnail)
         return thumbnail
-
+    
+    def get_streams(self):
+        streams = {}
+        fmt_stream_map = self._video_info['url_encoded_fmt_stream_map'][0].decode('utf-8').split(',')
+        # parse again because youtube encodes twice
+        entries = [parse_qs(entry) for entry in fmt_stream_map]  
+        for i in range(len(entries)): 
+            streams[((entries[i])['url'][0])] = ((entries[i])['itag'][0])
+        return streams
+        
 if __name__ == '__main__':
-    vinfo = VideoData('https://www.youtube.com/watch?v=EgqUJOudrcM')
+    vinfo = VideoData('https://www.youtube.com/watch?v=ObiqJzfyACM')
     print(vinfo.get_title())
-    print(vinfo.get_thumbnail_url(True))
+    print(vinfo.get_thumbnail_url('hq'))
+    print(vinfo.get_streams()) 
