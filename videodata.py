6c1aacaf9a72c0db5cdb33ed3e9f972c232301f7
Eric Ofman <eric.ofman@hotmail.com>
2016-02-20 02:40:26 -0500

    stream download

--- videodata.py
+++ videodata.py
@@ -2,9 +2,46 @@ from urllib.parse import urlparse
 from urllib.parse import parse_qs 
 from urllib.parse import urlencode 
 from urllib.request import urlopen 
+from urllib.request import urlretrieve
 import sys
 
 class VideoData:
+    fmt_types = {
+            5:  "FLV,320x240",         13: "3GP,176x144", 
+            17: "3GP,176x144",         18: "MP4,480x360", 
+            22: "MP4,1280x720",        34: "FLV,480x360",
+            35: "FLV,640x480",         36: "3GP,320x240", 
+            37: "MP4,1920x1080",       38: "MP4,2048x1080", 
+            44: "WEBM,640x480",        46: "WEBM,1920x1080",
+            59: "MP4,640x480",         78: "MP4,640x480", 
+            82: "MP4,480x360,3",       17: "3GP,176x144", 
+            18: "MP4,480x360",         22: "MP4,1280x720",
+            34: "FLV,480x360",         35: "FLV,640x480", 
+            36: "3GP,320x240",         37: "MP4,1920x1080", 
+            38: "MP4,2048x1080",       43: "WEBM,480x360",
+            44: "WEBM,640x480",        45: "WEBM,1280x720", 
+            46: "WEBM,1920x1080",      59: "MP4,640x480", 
+            78: "MP4,640x480",         82: "MP4,480x360,3",
+            83: "MP4,640x480,3",       84: "MP4,1280x720,3", 
+            85: "MP4,1920x1080,3",     100: "WEBM,480x360,3", 
+            101: "WEBM,640x480,3",     102: "WEBM,1280x720,3",
+           133: "MP4,320x240,V",       134: "MP4,480x360,V", 
+           135: "MP4,640x480,V",       136: "MP4,1280x720,V", 
+           137: "MP4,1920x1080,V",     139: "MP4,Low bitrate,A", 
+           140: "MP4,Med bitrate,A",   141: "MP4,Hi  bitrate,A",
+           160: "MP4,256x144,V",       171: "WEBM,Med bitrate,A", 
+           172: "WEBM,Hi  bitrate,A",  242: "WEBM,320x240,VO",
+           243: "WEBM,480x360,VO",     244: "WEBM,640x480,VO", 
+           245: "WEBM,640x480,VO",     246: "WEBM,640x480,VO",
+           247: "WEBM,1280x720,VO",    248: "WEBM,1920x1080,VO",
+           249: "WEBM,Low bitrate,AO", 250: "WEBM,Med bitrate,AO",
+           251: "WEBM,High bitrate,AO",264: "MP4,2560x1440,V",
+           266: "MP4,3840x2160,V",     271: "WEBM,2560x1440,VO",
+           272: "WEBM,3840x2160,VO",   278: "WEBM,256x144,VO",
+           298: "MP4,1280x720,V",      302: "WEBM,1280x720,VO",
+           313: "WEBM,3840x2160,VO",
+    }    
+    
     def __init__(self, video_url):
         self._video_url = video_url
         
@@ -51,15 +88,54 @@ class VideoData:
     
     def get_streams(self):
         streams = {}
-        fmt_stream_map = self._video_info['url_encoded_fmt_stream_map'][0].decode('utf-8').split(',')
+        fmt_stream_map = self._video_info['url_encoded_fmt_stream_map'][0] \
+            .decode('utf-8').split(',')
         # parse again because youtube encodes twice
         entries = [parse_qs(entry) for entry in fmt_stream_map]  
         for i in range(len(entries)): 
-            streams[((entries[i])['url'][0])] = ((entries[i])['itag'][0])
+            streams[((entries[i])['itag'][0])] = ((entries[i])['url'][0])
         return streams
+    
+    def _available_itags(self, streams):
+        return [int(value) for value in list(streams.keys())]
+    
+    def get_available_streams(self, streams):
+        itags = self._available_itags(streams) 
+        for itag in itags:
+            print("{0}: {1}".format(itag, self.fmt_types[itag])) 
+    
+    def download_stream(self, streams, itag, filename):
+        if not streams:
+            sys.exit("No streams available for this video")
+        
+        if itag not in self._available_itags(streams):
+            sys.exit("itag not found") 
+         
+        request = urlopen(streams[str(itag)])  
+        meta = request.info() 
+        file_size = int(meta.get_all("Content-Length")[0])      
+
+        file = open(filename + '.' + 
+                    self.fmt_types[itag].split(',')[0].lower(), "wb") 
+        blocksize = 8192
+        file_dl_size = 0               
+        while True:
+            chunk = request.read(blocksize)
+            if not chunk: break
+            file.write(chunk)
+            file_dl_size += len(chunk)  
+            print("Downloading: {0}/{1} Bytes".format(file_dl_size, file_size))
+        file.close() 
+        
+        if(file_dl_size == file_size):
+            print("Video successfully downloaded.")
+        else:
+            sys.exit("Video failed to properly download.")
         
 if __name__ == '__main__':
-    vinfo = VideoData('https://www.youtube.com/watch?v=ObiqJzfyACM')
+    vinfo = VideoData('https://www.youtube.com/watch?v=GQQMLE4FuIQ')
     print(vinfo.get_title())
     print(vinfo.get_thumbnail_url('hq'))
-    print(vinfo.get_streams()) 
+    streams = vinfo.get_streams()  
+    vinfo.get_available_streams(streams)
+    vinfo.download_stream(streams, 22, "video_test")
